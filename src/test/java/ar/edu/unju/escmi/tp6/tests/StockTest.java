23ac03af71e54d045d24a59452dbe04fdca01537
PC <PC@MaxStreet-PC>
2024-10-08 22:01:15 -0300

    Se desarrolla Credito y Stock test - Andreu Luca

---
+++ src/test/java/ar/edu/unju/escmi/tp6/tests/StockTest.java
@@ -0,0 +1,25 @@
+package ar.edu.unju.escmi.tp6.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import ar.edu.unju.escmi.tp6.collections.CollectionProducto;
+import ar.edu.unju.escmi.tp6.collections.CollectionStock;
+
+class StockTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+		CollectionProducto.precargarProductos();
+        CollectionStock.precargarStocks();
+	}
+
+	@Test
+	void test() {
+		CollectionStock.reducirStock(CollectionStock.stocks.get(0), 10);
+		assertEquals(CollectionStock.stocks.get(0).getCantidad(), 2);
+	}
+
+}
